3df153cdde69310bed287721bc3a3dedaac470c1
saurabh-gamare <sgamare32@gmail.com>
2023-09-12 00:40:47 +0530

    settings changes

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = Config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'sgamare32.pythonanywhere.com']
 
 # Application definition
 
@@ -114,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
