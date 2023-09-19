7335327cd815f65338a817332b7396aa0c54fddf
saurabh-gamare <sgamare32@gmail.com>
2023-09-19 12:33:09 +0530

    added extra columns in product model admin

--- ecom/products/admin.py
+++ ecom/products/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from . import models
 
 
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'mrp', 'sale_price', 'stock_qty')
+
+
+admin.site.register(models.Product, ProductModelAdmin)
 admin.site.register(models.Category)
-admin.site.register(models.Product)
