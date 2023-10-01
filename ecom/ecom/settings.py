1e6373c86334101fb6a2c4be12f889ef2192f8a2
saurabh-gamare <sgamare32@gmail.com>
2023-10-01 23:06:23 +0530

    updating cors settings

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -63,6 +63,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://127.0.0.1:8000',
     'http://192.168.151.116:3000',
+    'http://192.168.234.116:3000',
+    'http://localhost:3000',
 ]
 
 CORS_ALLOW_HEADERS = [
